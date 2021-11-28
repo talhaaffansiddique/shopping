247459acf29e0005594a14c4b44610832e59ce0a
talhaaffansiddique <talhaaffansiddique@gmail.com>
2021-11-28 10:17:46 +0500

    Update App.js
    border changes

--- App.js
+++ App.js
@@ -6,12 +6,21 @@ export default function App() {
   return (
     <View
       style={{
-        width: 100,
-        height: 100,
-        backgroundColor: "red",
-        alignItems: "center",
+        flex: 1,
         justifyContent: "center",
+        alignItems: "center",
       }}
-    ></View>
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+        }}
+      />
+    </View>
   );
 }
