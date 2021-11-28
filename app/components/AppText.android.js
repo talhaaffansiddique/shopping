fd8f20afd0cc44cb9704a22634ad3c38f28883e5
talhaaffansiddique <talhaaffansiddique@gmail.com>
2021-11-28 14:29:04 +0500

    4.8 Platform-specific Code

---
+++ app/components/AppText.android.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+function AppText({ children }) {
+  return <Text style={styles.text}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "tomato",
+    fontSize: 10,
+    fontFamily: "Roboto",
+  },
+});
+export default AppText;
