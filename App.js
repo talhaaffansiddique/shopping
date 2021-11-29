54132081d31e2a9a27011fce631292dc0f3a17fe
talhaaffansiddique <talhaaffansiddique@gmail.com>
2021-11-29 16:36:53 +0500

    4.14 Building the Listing Details Screen

--- App.js
+++ App.js
@@ -4,21 +4,8 @@ import { View, StyleSheet } from "react-native";
 import AppButton from "./app/components/AppButton";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red Jacket for sale"
-        subTitle="$100"
-        image={require("./assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
