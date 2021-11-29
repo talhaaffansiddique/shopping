3dc59c625f4caddc50e93612dbab6f7cf4242741
talhaaffansiddique <talhaaffansiddique@gmail.com>
2021-11-29 12:19:00 +0500

    4.13 Building the Card Component

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import AppButton from "./app/components/AppButton";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("./assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
